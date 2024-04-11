513973b2f9ced487752ac97b76937e195276fff0
luffy415 <divyanshulawda@gmail.com>
2024-04-11 14:52:31 +0530

    initial

---
+++ main.js
@@ -0,0 +1,6 @@
+function toggle() {
+  const menu = document.querySelector('.menu-lings');
+  const icon = document.querySelector('.ham-icon');
+  menu.classList.toggle('open');
+  icon.classList.toggle('open');
+}
