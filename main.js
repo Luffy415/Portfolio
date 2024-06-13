e71f242e638a9b86710d72dba1ebc7840008531c
Luffy415 <divyanshulawda@gmail.com>
2024-06-13 18:28:09 +0530

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-function toggle() {
-  const menu = document.querySelector('.menu-lings');
-  const icon = document.querySelector('.ham-icon');
-  menu.classList.toggle('open');
-  icon.classList.toggle('open');
-}
